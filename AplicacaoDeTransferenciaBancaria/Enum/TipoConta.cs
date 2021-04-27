d231aaa13d132902b81a11cef6dcd6e554b4589a
Vitor <vitor1184@hotmail.com>
2021-04-27 11:48:36 -0300

    Iniciando o projeto adicionando a classe Conta e o Enum para a tipagem das contas

---
+++ AplicacaoDeTransferenciaBancaria/Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+﻿namespace AplicacaoDeTransferenciaBancaria.Enum
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ AplicacaoDeTransferenciaBancaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using AplicacaoDeTransferenciaBancaria.Classes;
+
+namespace AplicacaoDeTransferenciaBancaria
+{
+    class Program
+    {
+		static void Main(string[] args)
+        {
+            
+        }
+	}
+}
